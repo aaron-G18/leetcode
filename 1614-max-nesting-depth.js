64df6edcf1d62dd355b78dc73e79c715ab65241c
Aaron Garner <aaron.garner.1982@gmail.com>
2020-11-08 21:35:42 -0800

    updated 1614 with solution.

--- 1614-max-nesting-depth.js
+++ 1614-max-nesting-depth.js
@@ -50,6 +50,23 @@
 //  * @return {number}
 //  */
 
-var maxDepth = function(s) {
-    
+const maxDepth = (s) => {
+    let max = 0;
+    let count = 0;
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === "(") {
+            count++;
+            if (count > max) {
+                max = count;
+            }
+        } else if (s[i] === ")") {
+            count--;
+        }
+        
+    };
+    console.log(max);
+    return max;
 };
+
+const s = "(1+(2*3)+((8)/4))+1";
+maxDepth(s);
