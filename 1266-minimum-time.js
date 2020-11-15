f2af129e0c810b9e2b20aa46ad174554ee68008b
Aaron Garner <aaron.garner.1982@gmail.com>
2020-11-15 14:24:10 -0800

    updated 1266 with second solution.

--- 1266-minimum-time.js
+++ 1266-minimum-time.js
@@ -49,19 +49,37 @@
 //  * @return {number}
 //  */
 
+// const minTimeToVisitAllPoints = (points) => {
+//     let coordArr = points;
+//     let sum = 0;
+//     for(let i = 0; i < coordArr.length-1; i++) {
+//         let currentCoords = [];
+//         let nextCoords = [];
+//         let xDiff = 0;
+//         let yDiff = 0;
+//         currentCoords = coordArr[i];
+//         nextCoords = coordArr[i+1];
+//         xDiff = Math.abs(currentCoords[0] - (nextCoords[0]));
+//         yDiff = Math.abs(currentCoords[1] - nextCoords[1]);
+//         sum += Math.max(xDiff, yDiff);
+//     };
+//     return sum;
+// };
+
+
+
+// submission results for the above solution:
+
+// Runtime: 80 ms, faster than 76.23% of JavaScript online submissions for Minimum Time Visiting All Points.
+// Memory Usage: 40 MB, less than 49.12% of JavaScript online submissions for Minimum Time Visiting All Points.
+
+
+// Goint to see if I can use less memory by trimming out some of the variables...
+
 const minTimeToVisitAllPoints = (points) => {
-    let coordArr = points;
     let sum = 0;
-    for(let i = 0; i < coordArr.length-1; i++) {
-        let currentCoords = [];
-        let nextCoords = [];
-        let xDiff = 0;
-        let yDiff = 0;
-        currentCoords = coordArr[i];
-        nextCoords = coordArr[i+1];
-        xDiff = Math.abs(currentCoords[0] - (nextCoords[0]));
-        yDiff = Math.abs(currentCoords[1] - nextCoords[1]);
-        sum += Math.max(xDiff, yDiff);
+    for(let i = 0; i < points.length-1; i++) {
+        sum += Math.max((Math.abs(points[i][0] - (points[i+1][0]))), (Math.abs(points[i][1] - points[i+1][1])));
     };
     return sum;
 };
