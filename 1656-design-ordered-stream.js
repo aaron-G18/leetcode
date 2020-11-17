048f18726036bfe36bf987d6346b74ebe220476b
Aaron Garner <aaron.garner.1982@gmail.com>
2020-11-16 16:16:32 -0800

    Updated 1656 with submission results.

--- 1656-design-ordered-stream.js
+++ 1656-design-ordered-stream.js
@@ -69,3 +69,8 @@ OrderedStream.prototype.insert = function(id, value) {
     return res;
 };
 
+// // Submission resultes:
+// Success
+// Details 
+// Runtime: 168 ms, faster than 100.00% of JavaScript online submissions for Design an Ordered Stream.
+// Memory Usage: 50.6 MB, less than 67.11% of JavaScript online submissions for Design an Ordered Stream.
