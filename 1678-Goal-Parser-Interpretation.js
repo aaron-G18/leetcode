f131058f2f6b19d5f9b362c5930ea36731f40164
Aaron Garner <aaron.garner.1982@gmail.com>
2020-12-27 15:04:33 -0800

    updated with solution.

--- 1678-Goal-Parser-Interpretation.js
+++ 1678-Goal-Parser-Interpretation.js
@@ -34,6 +34,16 @@
 //  * @param {string} command
 //  * @return {string}
 //  */
-var interpret = function(command) {
-    
+const interpret = (command) => {
+    let final = "";
+    for (let i=0; i<command.length; i++) {
+        if (command[i] === "G") {
+            final += "G";
+        } else if (command[i] === "(" && command[i+1] === ")") {
+            final += "o";
+        } else if (command[i] === "(" && command[i+1] === "a") {
+            final += "al";
+        }
+    };
+    return final;
 };
