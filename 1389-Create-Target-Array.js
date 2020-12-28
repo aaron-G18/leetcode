7f5f14d4b5ebdeb56b9316b1a1dbc4586a11398b
Aaron Garner <aaron.garner.1982@gmail.com>
2020-12-27 16:19:49 -0800

    updated with solution.

--- 1389-Create-Target-Array.js
+++ 1389-Create-Target-Array.js
@@ -51,6 +51,10 @@
 //  * @param {number[]} index
 //  * @return {number[]}
 //  */
-var createTargetArray = function(nums, index) {
-    
+const createTargetArray = (nums, index) => {
+    let targetArr = [];
+    for (let i=0; i<nums.length; i++) {
+        targetArr.splice(index[i], 0, nums[i]);
+    };
+    return targetArr;
 };
