d823f34dfa1a5c6431a70bc0f635b3a75f6fbb1d
Aaron Garner <aaron.garner.1982@gmail.com>
2020-12-29 15:39:44 -0800

    cleaned up spacing.

--- 1221-Split-Balanced-Strings.js
+++ 1221-Split-Balanced-Strings.js
@@ -6,23 +6,24 @@
 
 // Return the maximum amount of splitted balanced strings.
 
- 
-
 // Example 1:
 
 // Input: s = "RLRRLLRLRL"
 // Output: 4
 // Explanation: s can be split into "RL", "RRLL", "RL", "RL", each substring contains same number of 'L' and 'R'.
+
 // Example 2:
 
 // Input: s = "RLLLLRRRLR"
 // Output: 3
 // Explanation: s can be split into "RL", "LLLRRR", "LR", each substring contains same number of 'L' and 'R'.
+
 // Example 3:
 
 // Input: s = "LLLLRRRR"
 // Output: 1
 // Explanation: s can be split into "LLLLRRRR".
+
 // Example 4:
 
 // Input: s = "RLRRRLLRLL"
