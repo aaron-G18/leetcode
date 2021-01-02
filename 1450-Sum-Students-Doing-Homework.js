23d2ec9d8aef3c3e9d9c9440517b2de68b36c31f
Aaron Garner <aaron.garner.1982@gmail.com>
2021-01-02 15:27:53 -0800

    updated with solution.

--- 1450-Sum-Students-Doing-Homework.js
+++ 1450-Sum-Students-Doing-Homework.js
@@ -48,6 +48,12 @@
 //  * @param {number} queryTime
 //  * @return {number}
 //  */
-var busyStudent = function(startTime, endTime, queryTime) {
-    
+const busyStudent = (startTime, endTime, queryTime) => {
+    let total = 0;
+    for (let i = 0; i < startTime.length; i++) {
+        if (queryTime >= startTime[i] && queryTime <= endTime[i]) {
+            total++;
+        };
+    };
+    return total;
 };
